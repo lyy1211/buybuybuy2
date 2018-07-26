712803fec8fb4a305c66745ba5411ffbba880f3c
LYY <you@example.com>
2018-07-26 19:53:07 +0800

    主页完成

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
-Vue.use(VueRouter)
+
+
+Vue.use(VueRouter);
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 import index from './components/index.vue'
 const router = new VueRouter({
   routes :[
-    { path: '/', component: index },
+    { path: '/', redirect: '/index' },
     { path: '/index', component: index },
   ]
 })
